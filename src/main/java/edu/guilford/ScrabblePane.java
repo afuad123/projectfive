557999bd773467448e80f691b0b91c5e1ec95304
afuad727 <afuad@guilford.edu>
2024-04-05 12:19:54 -0400

    done i give up

--- src/main/java/edu/guilford/ScrabblePane.java
+++ src/main/java/edu/guilford/ScrabblePane.java
@@ -4,13 +4,15 @@ package edu.guilford;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import edu.guilford.BadWordException;
 
-public class ScrabblePane extends GridPane {
+public class ScrabblePane extends Pane {
 
     private Word word;
     private ScrabbleSet scrabbleset;
     private int[] letterCount;
+    private Pane scrabblePane;
     
 
     public ScrabblePane() {
@@ -19,7 +21,6 @@ public class ScrabblePane extends GridPane {
 
     public void drawScore() {
         //clear the pane
-        this.getChildren().clear();
         //get the score of the word
         int score = word.getScore();
         //display the score of the word using a label object
@@ -27,12 +28,12 @@ public class ScrabblePane extends GridPane {
         Label scoreLabel = new Label("The score of the word " + stringword.toUpperCase() + " is " + score);
         scoreLabel.setLayoutX(0);
         scoreLabel.setLayoutY(20);
-        //add the label to the pane
+        //add the label to the pane (not gridpane)
         this.getChildren().add(scoreLabel);
+
     }
 
     public void drawEmptyException() {
-        this.getChildren().clear();
         //get the value of the word
         //check if the word is empty
         Label emptyException = new Label("The word is empty");
@@ -42,7 +43,6 @@ public class ScrabblePane extends GridPane {
         this.getChildren().add(emptyException);   
     }
     public void drawNumberException() {
-        this.getChildren().clear();
         //get the value of the word
         Label numberException = new Label("The word entered contains a number");
         numberException.setLayoutX(0);
@@ -52,23 +52,21 @@ public class ScrabblePane extends GridPane {
     }
 
     public void drawInvalidCharacterException() {
-        this.getChildren().clear();
         //get the value of the word
         //loop that parses through the word and identifies invalid characters
         Label invalidCharacterException = new Label("The word entered contains an invalid character");
         invalidCharacterException.setLayoutX(0);
-        invalidCharacterException.setLayoutY(20);
+        invalidCharacterException.setLayoutY(40);
         //add the label to the pane
         this.getChildren().add(invalidCharacterException); 
     }
 
     public void drawInvalidWordException () {
-        this.getChildren().clear();
         //get the value of the word
         Label invalidWordException = new Label("The word is not a valid word in the English language");
         invalidWordException.setLayoutX(0);
+        invalidWordException.setLayoutY(40);  
         this.getChildren().add(invalidWordException);
-        invalidWordException.setLayoutY(20);  
         }
 
     public void setWord(Word word) {
@@ -78,16 +76,6 @@ public class ScrabblePane extends GridPane {
     public void setScrabbleSet(ScrabbleSet scrabbleset) {
         this.scrabbleset = scrabbleset;
     }
-
-    //setLanguageLabel
-    public void setLanguageLabel(String language) {
-        Label languageLabel = new Label("The language of the word is " + language);
-        languageLabel.setLayoutX(0);
-        languageLabel.setLayoutY(0);
-        //add the label to the pane
-        this.getChildren().add(languageLabel);
-        languageLabel.setText(language);
-        }
 
    
     }
